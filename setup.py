88cd31eb39a5c5ea35bd6bdd26688b87a51f2250
Grey41 <hello@greyhope.uk>
2022-08-12 10:43:11 +0100

    New version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ elif sys.platform == "darwin":
 
 setuptools.setup(
     name = "JoBase",
-    version = "2.2",
+    version = "2.3",
     author = "Reuben Ford",
     author_email = "hello@jobase.org",
     description = "Fast Python Game Library",
